ed326f86d8c8017f45ec05701a1b4cbb3717bc60
thibauttaglialatela <thibauttaglialatela@gmail.com>
2023-09-12 16:58:14 +0200

    📦 NEW: add pokemon list

--- src/Components/PokemonCard.jsx
+++ src/Components/PokemonCard.jsx
@@ -1,8 +1,19 @@
+const pokemonList = [
+    {
+        name: 'Bulbasaur',
+        imgSrc: 'https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/1.svg'
+    },
+    {
+        name: 'Mew'
+    }
+]
+
 function PokemonCard() {
+    let pokemon = pokemonList[0];
     return (
 <figure>
-    <img src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/1.svg" alt="bulbasaur" />
-    <figcaption>Bulbasaur</figcaption>
+    {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+    <figcaption>{pokemon.name}</figcaption>
 </figure>
     )
 }
