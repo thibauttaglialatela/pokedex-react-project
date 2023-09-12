18476e781bf08c3327ee7aaadcd2cd870803ee93
thibauttaglialatela <thibauttaglialatela@gmail.com>
2023-09-12 15:41:16 +0200

    📦 NEW: create PokemonCard

---
+++ src/Components/PokemonCard.jsx
@@ -0,0 +1,10 @@
+function PokemonCard() {
+    return (
+<figure>
+    <img src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/1.svg" alt="bulbasaur" />
+    <figcaption>Bulbasaur</figcaption>
+</figure>
+    )
+}
+
+export default PokemonCard;
